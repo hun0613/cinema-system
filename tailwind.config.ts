f77f61832b13c551ad1b45a218eae0dd70c5d98d
hun0613 <pshun7614@gmail.com>
2024-02-04 23:16:02 +0900

    setting/prettier 설정 및 local font 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        NMSNeo1: ["NMSNeo1"],
+        NMSNeo2: ["NMSNeo2"],
+        NMSNeo3: ["NMSNeo3"],
+        NMSNeo4: ["NMSNeo4"],
+        NMSNeo5: ["NMSNeo5"],
+        NMSNeo6: ["NMSNeo6"],
+      },
+      colors: {
+        bgColor: "#1E1E1E",
+      },
     },
   },
   plugins: [],
