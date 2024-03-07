1433c165bc4d080fd40e33cf341c0e6bb019e2de
hun0613 <pshun7614@gmail.com>
2024-03-07 23:31:34 +0900

    cors 에러 대응 (nextconfig변경)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/book/api/theater",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
