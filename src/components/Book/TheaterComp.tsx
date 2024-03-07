b1e7bac5ecd35c4c11a9d598c255c2638c3be54f
hun0613 <pshun7614@gmail.com>
2024-03-08 00:01:09 +0900

    cors 에러 대응 (nextconfig 수정)

--- src/components/Book/TheaterComp.tsx
+++ src/components/Book/TheaterComp.tsx
@@ -35,7 +35,7 @@ const TheaterComp = ({
   useEffect(() => {
     // 첫 랜더링 시 theater data fetch
     if (!theaterDb) {
-      fetch(`${process.env.NEXT_PUBLIC_API}/book/api/theater`)
+      fetch(`/book/api/theater`)
         .then((res) => res.json())
         .then((res2) => {
           setTheaterDb(res2);
