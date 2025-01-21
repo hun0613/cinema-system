8d9107bcb36dfb345b97daa5f17a7b191a4f4f20
hun0613 <pshun7614@gmail.com>
2025-01-22 00:59:43 +0900

    fix/axios cors에러 대응(4차시도)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,9 +6,8 @@ export type UseMutationAction<TData, TResult = void> = (
 ) => UseMutationResult<TData, AxiosError, TResult>;
 
 export const header = {
-  "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
   "Access-Control-Allow-Headers":
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    "Origin, X-Auth-Token, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
 };
