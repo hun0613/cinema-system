f2b822dfe0a38309fbe5db2034049a1fda574cc6
hun0613 <pshun7614@gmail.com>
2025-01-22 01:12:26 +0900

    fix/axios cors에러 대응(5차시도)

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/book/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
